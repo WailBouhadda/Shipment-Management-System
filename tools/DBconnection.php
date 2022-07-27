0ad1eaa1cc00b578d37044bab923e0712b38c518
WailBouhadda <wailbouhadda1@gmail.com>
2022-07-27 21:44:52 +0100

    update login title

--- tools/DBconnection.php
+++ tools/DBconnection.php
@@ -7,8 +7,8 @@ private $conn = null;
 
  public function connect(){
 
-  $conn = new mysqli("	sql107.epizy.com","	epiz_32192946","wGcGi1qJu7QKjMB","epiz_32192946_shipmentmanagement");
-
+  //$conn = new mysqli("sql107.epizy.com","epiz_32192946","wGcGi1qJu7QKjMB","epiz_32192946_shipmentmanagement");
+  $conn = new mysqli("localhost","root","","shipmentmanagement");
 
   if ($conn->connect_error) {
 
